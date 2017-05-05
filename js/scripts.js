bec8ef374b7ffa6917bac3bfbd71e1dfdb53ef71
Kristen <kristen.dynamite@gmail.com>
2017-05-05 13:03:09 -0700

    create functions and conditions for all specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
+
+
+// // // // // back-end business logic // // // // //
+function pingPong(userInput) {
+  var newArray = [];
+  for (var i = 1; i < userInput; i++) {
+    if (i % 15 === 0) {
+      newArray.push("ping-pong");
+    } else if (i % 3 === 0) {
+      newArray.push ("ping");
+    } else if (i % 5 === 0) {
+      newArray.push ("pong");
+    } else {
+      newArray.push (userInput.toString());
+      // newArray = newArray.toString().split(" ");
+    }
+  }
+}
 // // // // // front-end user interface logic // // // // //
 $(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#number").val();
-
-    if (userInput === "") {
-      alert('Please enter a number to play')
-      $("#output").hide();
-      }
-
-    $(".number").text(userInput)
+    var userInput = parseInt($("#number").val());
+    //
+    // if (userInput === "") {
+    //   alert('Please enter a number to play')
+    //   return;
+    //   }
+    var result = pingPong(userInput);
+    $(".resultNumbers").text(result);
 
     $("#output").show();
 
   });
 });
-
-// // // // // back-end business logic // // // // //
-// var parseInput = parseInt($("#number").val());
-// var newArray = [];
-//
-// if (parseInput % 3 = 0 && parseInput % 5 = 0) {
-//   newArray.push(["ping-pong"]);
-// } else if (parseInput % 3 = 0) {
-//   newArray.push(["ping"]);
-// } else if (parseInput % 5 = 0) {
-//   newArray.push(["pong"]);
-// }
