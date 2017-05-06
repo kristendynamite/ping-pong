80abf31a5f308e2b3001f0b0be3290713df2b714
Kristen <kristen.dynamite@gmail.com>
2017-05-05 17:00:44 -0700

    change strategy, remove for each loop and use for loop instead with alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,25 @@
-
-
-// // // // // back-end business logic // // // // //
-function pingPong(userInput) {
-  var parsedInput = parseInt(userInput);
-
-  var newArray = [];
-  for (var multiple = 1; multiple <= parsedInput; multiple += 1) {
-    if (multiple % 15 === 0) {
-      newArray.push("ping-pong");
-    } else if (multiple % 3 === 0) {
-      newArray.push ("ping");
-    } else if (multiple % 5 === 0) {
-      newArray.push ("pong");
-    } else {
-      newArray.push (multiple);
-      // newArray = newArray.toString().split(" ");
-    }
-    // newArray = newArray.toString();
-    return newArray;
-  }
-}
-// // // // // front-end user interface logic // // // // //
-$(function() {
+       //front-end user interface logic // // // // //
+$(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#number").val();
 
+      // back-end business logic
+    var userInput = parseInt($("#number").val());
+  for (var multiple = 1; multiple <= userInput; multiple += 1) {
+    if (multiple % 15 === 0) {
+      alert ("ping-pong");
+    } else if (multiple % 3 === 0) {
+      alert ("ping");
+    } else if (multiple % 5 === 0) {
+      alert ("pong");
+    } else {
+      multiple = multiple;
+      alert (multiple);
+    }
+  }
     if (userInput === "") {
       alert('Please enter a number to play')
       return;
       }
-    var results = pingPong(userInput);
-
-    results.forEach(function(result){
-      $("#result-numbers").prepend("<li>" + "</li>");
-    });
-
-    $("#output").show();
-
   });
 });
